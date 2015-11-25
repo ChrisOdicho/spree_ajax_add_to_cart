fc1a093c56114a07f293e0cc50cbf33d613be84a
Chris Odicho <chris.odicho@gmail.com>
2015-11-25 21:43:34 +1100

    More fixes

--- app/overrides/ajax_cart_form.rb
+++
@@ -1,4 +0,0 @@
-Deface::Override.new(:virtual_path => 'spree/products/_cart_form',
-                     :name => 'remote_cart_form',
-                     :replace => "code[erb-loud]:contains('form_for :order, :url => populate_orders_url do |f|')",
-                     :erb => "<%= form_for :order, :url => populate_orders_path, :remote => true do |f| %>")
